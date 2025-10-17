be86f8429f74321ab25cd323ac3a53dcb5f8c7e1
letuananh127 <letuananh1207204@gmail.com>
2025-10-17 22:53:26 +0700

    chinh lai overflowed

--- lib/features/home/ui/widgets/quick_actions.dart
+++ lib/features/home/ui/widgets/quick_actions.dart
@@ -30,13 +30,13 @@ class QuickActions extends StatelessWidget {
                 label: 'Chuyển thời gian',
                 onTap: onTransfer,
               ),
-              const SizedBox(width: 50),
+              const SizedBox(width: 40),
               MenuButton(
                 icon: Icons.qr_code_scanner_rounded,
                 label: 'QR',
                 onTap: onQr,
               ),
-              const SizedBox(width: 50),
+              const SizedBox(width: 40),
               MenuButton(
                 icon: Icons.history,
                 label: 'Lịch sử\nGiao dịch',
