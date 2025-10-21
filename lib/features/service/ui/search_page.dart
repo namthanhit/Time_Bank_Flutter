ee738f14c95889c37869b575e12b4c343285e847
Hieuab1308 <22010104@st.phenikaa-uni.edu.vn>
2025-10-22 01:07:49 +0700

    feat: add basic search page

---
+++ lib/features/service/ui/search_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'widgets/service_list_container.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _searchController = TextEditingController();
+  String _query = '';
+  bool _isSearching = false;
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF8F9FA),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF003E77),
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+        ),
+        title: Container(
+          height: 40,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: TextField(
+            controller: _searchController,
+            autofocus: true,
+            onChanged: (value) {
+              setState(() {
+                _query = value;
+                _isSearching = value.isNotEmpty;
+              });
+            },
+            decoration: InputDecoration(
+              hintText: 'Tìm kiếm dịch vụ...',
+              prefixIcon: Icon(Icons.search, color: Colors.grey[400], size: 20),
+              suffixIcon: _searchController.text.isNotEmpty
+                  ? IconButton(
+                      onPressed: () {
+                        _searchController.clear();
+                        setState(() {
+                          _query = '';
+                          _isSearching = false;
+                        });
+                      },
+                      icon:
+                          Icon(Icons.clear, color: Colors.grey[400], size: 20),
+                    )
+                  : null,
+              border: InputBorder.none,
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            ),
+          ),
+        ),
+      ),
+      body: _isSearching
+          ? ServiceListContainer(
+              query: _query,
+              filter: null,
+            )
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.search,
+                    size: 80,
+                    color: Colors.grey[400],
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Tìm kiếm dịch vụ',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.grey[600],
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Nhập từ khóa để tìm kiếm dịch vụ phù hợp',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[500],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
