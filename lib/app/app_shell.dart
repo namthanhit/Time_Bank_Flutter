88256d113555e5835b9bb65ca34e1949073e9f41
Hieuab1308 <22010104@st.phenikaa-uni.edu.vn>
2025-11-02 14:35:19 +0700

    adjust buttom navigation bar

--- lib/app/app_shell.dart
+++ lib/app/app_shell.dart
@@ -5,7 +5,6 @@ import 'navigation/bottom_nav_bar.dart';
 import 'navigation/nav_item_data.dart';
 import '../features/qr/ui/qr_scanner_page.dart';
 import '../features/settings/ui/settings_page.dart';
-import '../common/ui/stub_page.dart';
 import '../features/chat/ui/chat_list_page.dart';
 import '../features/time_transfer/ui/transfer_page.dart';
 
