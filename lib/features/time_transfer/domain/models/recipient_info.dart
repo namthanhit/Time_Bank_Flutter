f86f701da7fea4bccc86efe62a631ec79fdcef86
Thanh Nam <thanhnamit08@gmail.com>
2025-11-19 02:35:19 +0700

    fix avatar transfer

--- lib/features/time_transfer/domain/models/recipient_info.dart
+++ lib/features/time_transfer/domain/models/recipient_info.dart
@@ -1,13 +1,14 @@
 class RecipientInfo {
   final String id;
   final String fullName;
-
-  RecipientInfo({required this.id, required this.fullName});
+  final String? avatarUrl;
+  RecipientInfo({required this.id, required this.fullName, this.avatarUrl});
 
   factory RecipientInfo.fromJson(Map<String, dynamic> json) {
     return RecipientInfo(
       id: json['id'],
       fullName: json['full_name'],
+      avatarUrl: json['avatar_url'] is String ? json['avatar_url'] as String? : null,
     );
   }
 }
